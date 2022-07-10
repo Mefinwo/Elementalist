7a15d52ddd83805d10c1cb36d70667046b281197
Mefinwo <mefinwo@gmail.com>
2022-07-10 21:35:42 +0200

    second ocmmit

---
+++ script.js
@@ -0,0 +1,47 @@
+//List of all symbols
+const Elemente = ["H", "Li", "Na", "K", "Rb", "Cs", "Fr", "Be", "Mg", "Ca", "Sr", "Ba", "Ra", "B", "Al", "Ga", "In", "Tl", "C", "Si", "Ge",
+    "Sn", "Pb", "N", "P", "As", "Sb", "Bi", "O", "S", "Se", "Te", "Po", "F", "Cl", "Br", "I", "At", "He", "Ne", "Ar", "Kr", "Xe", "Rn"]
+
+//List of all elements
+const Elemente_name = ["Wasserstoff", "Lithium", "Natrium", "Kalium", "Rubidium", "Ceasium", "Francium", "Beryllium", "Magnesium", "Calcium", "Strontium", "Barium", "Radium", "Bor", "Aluminium", "Gallium", "Indium", "Thallium", "Kohlenstoff", "Silicium",
+    "Germanium", "Zinn", "Blei", "Stickstoff", "Phosphor", "Arsen", "Antimon", "Bismut", "Sauerstoff", "Schwefel", "Selen", "Tellur", "Polonium", "Flour", "Chlor", "Brom", "lod", "Astat", "Helium", "Neon", "Argon", "Kypton", "Xenon", "Radon"]
+
+//List of all main groups
+const Hauptgruppe = ["1", "1", "1", "1", "1", "1", "1", "2", "2", "2", "2", "2", "2", "3", "3", "3", "3", "3", "4", "4", "4",
+    "4", "4", "5", "5", "5", "5", "5", "6", "6", "6", "6", "6", "7", "7", "7", "7", "7", "8", "8", "8", "8", "8", "8"]
+
+//List of all atomic numbers
+const Ordnungszahl = ["1", "3", "11", "19", "37", "55", "87", "4", "12", "20", "38", "56", "88", "5", "13", "31", "49", "81", "6", "14", "32",
+    "50", "82", "7", "15", "33", "51", "83", "8", "16", "34", "52", "84", "9", "17", "35", "53", "85", "2", "10", "18", "36", "54", "86"]
+
+//List of Electronegativity values
+const En = ["2.2", "0.98", "0.93", "0.82", "0.82", "0.79", "0.7", "1.57", "1.31", "1.0", "0.95", "0.89", "0.89", "2.04", "1.61", "1.81", "1.78", "1.62", "2.55", "1.9", "2.01",
+    "1.96", "2.33", "3.04", "2.19", "2.18", "2.05", "2.02", "3.44", "2.58", "2.55", "2.66", "2.0", "3.98", "3.16", "2.96", "2.1", "2.2", "/", "/", "/", "/", "/", "/"]
+
+//List of atomic density
+const Dichte = ["0.09", "0.53", "0.97", "0.86", "1.53", "1.90", "?", "1.85", "1.74", "1.55", "2.63", "3.59", "5.50", "2.46", "2.70", "5.90", "7.31", "11.85", "2.26", "2.34", "5.32",
+    "7.26", "11.35", "1.25", "2.69", "5.73", "6.70", "9.75", "1.43", "2.07", "4.82", "6.25", "9.20", "1.70", "3.21", "3.12", "4.94", "?", "0.18", "0.90", "1.78", "3.75", "5.90", "9.73"]
+
+//List of main group 1
+const hg1 = ["H = Wasserstoff", "Li = Lithium", "Na = Natrium", "K = Kalium", "Rb = Rubidium", "Cs = Ceasium", "Fr = Francium"]
+
+//List of main group 2
+const hg2 = ["Be = Beryllium", "Mg = Magnesium", "Ca = Calcium", "Sr = Strontium", "Ba = Barium", "Ra = Radium"]
+
+//List of main group 3
+const hg3 = ["B = Bor", "Al = Aluminium", "Ga = Gallium", "In = Indium", "Ti = Thallium"]
+
+//List of main group 4
+const hg4 = ["C = Kohlenstoff", "Si = Silicium", "Ge = Germanium", "Sn = Zinn", "Pb = Blei"]
+
+//List of main group 5
+const hg5 = ["N = Stickstoff", "P = Phosphor", "As = Arsen", "Sb = Antimon", "Bi = Bismut"]
+
+// List of main group 6
+const hg6 = ["O = Sauerstoff", "S = Schwefel", "Se = Selen", "Te = Tellur", "Po = Polonium"]
+
+//List of main group 7
+const hg7 = ["F = Flour", "Cl = Chlor", "Br = Brom", "I = Iod", "At = Astat"]
+
+//List of main group 8
+const hg8 = ["He = Helium", "Ne = Neon", "Ar = Argon", "Kr = Krypton", "Xe = Xenon", "Rn = Radon"]
